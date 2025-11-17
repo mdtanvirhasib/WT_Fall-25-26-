9da93670cc0e3ae7eaed1cbdbf55a64af7fcaf68
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2025-11-17 11:42:42 +0600

    2nd

--- Mid/Lab tasks/Lab task 2/Lab2.php
+++ Mid/Lab tasks/Lab task 2/Lab2.php
@@ -2,12 +2,29 @@
 <html>
     <head>
         <title>Registration form</title>
+        <style>
+            .test{
+                width: 95%;
+                padding: 7px;
+            }
+        </style>
     </head>
 
     <body>
         <center>
            <table>
-                <h1>Student Registration</h1>
+                <h1>Student Registration</h1><br>
+                <tr><td>Full Name:</td></tr>
+                <tr><td><input type="text"></td></tr>
+                <tr><td>Email:</td></tr>
+                <tr><td><input type="email"></td></tr>
+                <tr><td>Password:</td></tr>
+                <tr><td><input type="password"></td></tr>
+                <tr><td>Confirm Password:</td></tr>
+                <tr><td><input type="password"></td></tr>
+
+
+
            </table> 
         </center>
         
