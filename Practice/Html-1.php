b1e55694c26d9d1c2982f7074bff1d6ce5a53b94
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2025-11-26 22:54:43 +0600

    push

---
+++ Practice/Html-1.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Tanvir Hasib's protfolio
+        </title>
+    </head>
+    <body>
+        <header>
+            <center>
+                <h1>Md. Tanvir Hasib Fahim</h1>
+                <nav>
+                    <a href="#bio">Biography </a> | 
+                    <a href="#edu">Education </a> | 
+                    <a href="#con">Contact</a>
+                </nav>
+            </center>
+        </header>
+        <main>
+            <center>
+
+            <section id="bio">
+                <h2>About me</h2>
+                <p>I am Tanvir Hasib. I am a computer science student. I am currently preparing for an internship to gain hands-on experience
+                in software development and real-world projects.</p>
+            </section>
+
+            <section id="edu">
+                <h2>Educational Qualifications</h2>
+
+                <table border="1" width="80%">
+                    <tr>
+                        <td style="font-weight: bold;">Degree</td>
+                        <td style="font-weight: bold;">Institution</td>
+                        <td style="font-weight: bold;">Year</td>
+                        <td style="font-weight: bold;">Result</td>
+                </tr>
+
+                <tr>
+                    <td>BSc in CSE</td>
+                    <td>American International University Bangladesh</td>
+                    <td>2023–Present</td>
+                    <td>Running</td>
+                </tr>
+
+                <tr>
+                    <td>HSC</td>
+                    <td>Feni Govt. College</td>
+                    <td>2021</td>
+                    <td>GPA 4.92</td>
+                </tr>
+
+                <tr>
+                    <td>SSC</td>
+                    <td>Feni Govt Pilot High School</td>
+                    <td>2019</td>
+                    <td>GPA 4.94</td>
+                </tr>
+            </table>
+        </section>
+
+        <section id="con">
+            <h2>Contact Me</h2>
+            <p>Email : <a href="mailto:tanvirhasib891@gmail.com">tanvirhasib891@gmail.com</a></p>
+            <p>Mobile : <a href="tel:01742084795">01742084795</a></p>
+        </section>
+        </center>
+        </main>
+
+        <footer>
+            <center>
+                <p>Md. Tanvir Hasib Fahim</p>
+            </center>
+        </footer>
+    </body>
+</html>
