00da233de8cfd83af00970eae0f08ae92e14728f
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2025-12-16 10:46:22 +0600

    1at

---
+++ Final/Project/User/View/userlogin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <form action="">
+            <h1>User Login</h1>
+            <table>
+                <tr>
+                    <td>
+                        <label for="">Email ID:</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label for="">Password:</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="password">
+                    </td>
+                </tr>
+            </table>
+
+            <button>Forgot Password?</button>
+
+            <input type="submit" value="Login">
+
+        </form>
+    </body>
+</html>
