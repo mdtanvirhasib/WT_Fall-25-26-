28126c4ce10ef9213123e0b6e49b47616f0457b8
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2025-12-23 20:53:39 +0600

    6

---
+++ Final/Project/User/View/dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Food ordering system</title>
+    </head>
+    <body>
+        <h1>Food ordering system</h1><br>
+        <a href="userlogin.php">Sign in</a>
+        <a href="userreg.php">Sign up</a>
+
+
+    </body>
+</html>
