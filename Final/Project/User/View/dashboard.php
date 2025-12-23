8f69e4c59e5c0ebb25bdcc1aca1bdaaf0595d0e5
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2025-12-23 22:01:13 +0600

    7

--- Final/Project/User/View/dashboard.php
+++ Final/Project/User/View/dashboard.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Food ordering system</title>
-    </head>
-    <body>
-        <h1>Food ordering system</h1><br>
-        <a href="userlogin.php">Sign in</a>
-        <a href="userreg.php">Sign up</a>
 
+<head>
+    <title>Food ordering system</title>
+    <link rel="stylesheet" href="../Css/deshboard.css">
+</head>
 
-    </body>
+<body>
+    <header>
+        <h1>Food ordering system</h1>
+        <a href="userlogin.php" id="btn1">Sign in</a>
+        <a href="userreg.php" id="btn2">Sign up</a>
+    </header>
+
+    <h2>Welcome</h2>
+
+    <footer>Food ordering system</footer>
+</body>
+
 </html>
